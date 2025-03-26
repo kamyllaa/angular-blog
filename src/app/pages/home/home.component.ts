d69138d9e448a6b166533d27b99951deacb0f50a
Kamylla Reis <112005494+kamyllaa@users.noreply.github.com>
2025-03-26 09:27:15 -0300

    adicionei comentários

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'; /* trazer funcionalidades prontas de bibliotecas
+ou o proprio angular */
 
-@Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
+@Component({ /* é um decorator qie transforma uma classe em um componente angular */
+  selector: 'app-home', /* define o nome da tag HTML para usar esse componente */
+  templateUrl: './home.component.html', /* o arquivo HTML que mostra o visual do componente
+  (como ele aparece para o usuário) */
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit { /* criando uma class chamada HomeComponent
+  e que essa classe vai usar a funcionalidade do Angular chamada ngOnInit */
 
-  constructor() { }
+  constructor() { } /* é chamado quando o componente é criado */
 
-  ngOnInit(): void {
+  ngOnInit(): void { /* esse método é chamado automaticamente quando o componente
+    é carregado na tela */
   }
 
 }
+/* Esse código está criando um componente Angular chamado HomeComponent, com HTML e CSS próprios. Ele ainda não tem lógica dentro, mas está pronto pra você colocar coisas como:
+
+Mostrar uma mensagem de boas-vindas
+
+Buscar dados de uma API
+
+Exibir listas, formulários, etc. */
